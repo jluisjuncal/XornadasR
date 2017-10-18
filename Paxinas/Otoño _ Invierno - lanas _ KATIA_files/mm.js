5e2cfbd75951fcf7771b2e70c85a69121aa1d562
J. Luis Juncal <jluisjuncal@gmail.com>
2017-10-18 15:17:57 +0200

    Páxinas de katia para presentación offline

---
+++ Paxinas/Otoño _ Invierno - lanas _ KATIA_files/mm.js
@@ -0,0 +1,60 @@
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v3.0
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+
+function flotant(pagina,nombre,ancho,largo,x,y,scr) {
+  var Ventana= 'toolbar=0,location=0,directories=0,scrollbars='+scr+',screenX='+x+'px,screenY='+y+'px,menubar=0,resizable=0,width='+ancho+',height='+largo+',status=0';
+  open(pagina,nombre,Ventana);
+}
+
+function GetNewRequest() {
+  try {
+    var request = new XMLHttpRequest();
+  } catch (trymicrosoft) {
+    try {
+      var request = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (othermicrosoft) {
+      try {
+        var request = new ActiveXObject("Microsoft.XMLHTTP");
+      } catch (failed) {
+        var request = false;
+      }
+    }
+  }
+
+  if (!request) {
+    alert("Sistema AJAX no operativo!");
+  }
+  return request;
+}
+
+function Toggle(capa) {
+  c = document.getElementById(capa);
+  if ( c.style.display == 'none' ) {
+    c.style.display = 'block';
+  } else {
+    c.style.display = 'none';
+  }
+}
+
+function Cerrar(capa) {
+  document.getElementById(capa).style.display = 'none';
+}
